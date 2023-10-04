0046641dc1181ee8355cec56fe89b59ec56803e9
Ignácz Dominik Bence <dominikignacz90@gmail.com>
2023-10-04 09:26:53 +0200

    Fix: Change Random to SecureRandom

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -1,6 +1,6 @@
 package hu.bme.mit.spaceship;
 
-import java.util.Random;
+import java.security.SecureRandom;
 
 /**
 * Class storing and managing the torpedoes of a ship
@@ -12,7 +12,7 @@ public class TorpedoStore {
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
-  private Random generator = new Random();
+  private SecureRandom generator = new SecureRandom();
   private int torpedoCount = 50;
 
   public TorpedoStore(int numberOfTorpedos){
